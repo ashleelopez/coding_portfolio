cfc8e1e52200b50e3155e6632e715a19182ce227
Ashlee Lopez Santiago <111544992+ashleelopez@users.noreply.github.com>
2023-03-21 21:55:24 +0000

    Tue, Mar 21, 2023, 2:55 PM -07:00

--- js/counter.js
+++ js/counter.js
@@ -12,6 +12,9 @@ counters.forEach(counter => {
             counter.innerText = `${Math.ceil(c + increment)}`
             setTimeout(updateCounter , 1)
         }
+        else {
+            counter.innerText = target
+        }
     }
     updateCounter()
 })
